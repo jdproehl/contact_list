33a2ee5e09d643d15e75686ab79414c3a0de2de0
Jaron Proehl <jaron@strategicmissionelements.com>
2024-01-03 20:55:13 -0500

    finished first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      
+      <ContactList />
     </>
-  )
+  );
 }
-
-export default App
